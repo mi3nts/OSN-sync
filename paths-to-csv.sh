77dd711f5227e8665863474be3c137cb06320b96
John Waczak <john.louis.waczak@gmail.com>
2023-06-28 00:06:44 -0500

    adding files for auto-generating links

--- paths-to-csv.sh
+++ paths-to-csv.sh
@@ -5,10 +5,10 @@ GIT=`which git`
 
 echo ${GIT}
 
-#rclone lsf  --format "tsp" --csv --absolute --files-only -R OSN:ees230012-bucket01/AirQualityNetwork/data/raw --exclude-from exclude-file.txt > raw-osn-links.csv
-rclone lsf  --format "tsp" --csv --absolute OSN:ees230012-bucket01/AirQualityNetwork/data/raw --exclude-from exclude-file.txt > raw-osn-links.csv
+rclone lsf  --format "tsp" --csv --absolute --files-only -R OSN:ees230012-bucket01/AirQualityNetwork/data/raw --exclude-from exclude-file.txt > raw-osn-links.csv
 
-cd ${REPO_DIR}
+# rclone lsf  --format "tsp" --csv --absolute OSN:ees230012-bucket01/AirQualityNetwork/data/raw --exclude-from exclude-file.txt > raw-osn-links.csv
+
 ${GIT} add raw-osn-links.csv
 ${GIT} commit -m "updating csv of raw-osn-links"
-#${GIT} push git@github.com:mi3nts/OSN-sync.git main
+${GIT} push git@github.com:mi3nts/OSN-sync.git main
